988173c10a19ac0164aab6c1292f1bf92956c80f
RATIU5 <jcmemmott20@outlook.com>
2024-09-11 07:51:30 -0600

    edit: nav for poverty

--- packages/medusa/src/admin/layouts/povertyLayout.tsx
+++ packages/medusa/src/admin/layouts/povertyLayout.tsx
@@ -1,25 +1,16 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { redirect } from "react-router-dom";
-import { Container, DropdownMenu, Button } from "@medusajs/ui";
+import { Link } from "react-router-dom";
+import { Container, Button } from "@medusajs/ui";
 
 const queryClient = new QueryClient();
 
 const PovertyLayout = ({ children }) => {
-  function handleNavigation() {
-    return new Response("", {
-      status: 302,
-      headers: {
-        Location: "/admin/poverty/navigation",
-      },
-    });
-  }
-
   return (
     <>
       <Container className="w-full">
-        <Button variant="transparent" onClick={handleNavigation}>
-          Navigation
-        </Button>
+        <Link to="/poverty/navigation">
+          <Button variant="transparent">Navigation</Button>
+        </Link>
         <Button variant="transparent">Something</Button>
         <Button variant="transparent">Else</Button>
       </Container>
